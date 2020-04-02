6e0f4de4885097bc3adf4e7a093ca79766b1bd1e
a1824jj <a1824jj@aiit.ac.jp>
2020-04-02 11:05:18 +0900

    add webservice class

---
+++ GoodWeather-sample/Services/Webservice.swift
@@ -0,0 +1,34 @@
+//
+//  Webservice.swift
+//  GoodWeather-sample
+//
+//  Created by JONGWOO JIN on 2020/04/02.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+
+struct Resource<T> {
+    let url: URL
+    let parse: (Data) -> T?
+}
+
+final class Webservice{
+    
+    func load<T>(resource: Resource<T>, completion: @escaping(T?)->()) {
+       
+        URLSession.shared.dataTask(with: resource.url) { data, response, error in
+                
+            if let data = data {
+                DispatchQueue.main.async {
+                     completion(resource.parse(data))
+                }
+            } else {
+                completion(nil)
+            }
+            
+        }.resume()
+    
+    }
+    
+}
