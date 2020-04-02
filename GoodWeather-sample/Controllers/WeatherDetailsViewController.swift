24b17a3bb80a6fba06abf576311b0cf9bb513e29
a1824jj <a1824jj@aiit.ac.jp>
2020-04-02 16:46:41 +0900

    binding values userinterface

--- GoodWeather-sample/Controllers/WeatherDetailsViewController.swift
+++ GoodWeather-sample/Controllers/WeatherDetailsViewController.swift
@@ -20,14 +20,22 @@ class WeatherDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupVMBindings()
+    }
+    
+    private func setupVMBindings(){
+
         if let weatherVM = self.weatherViewModel {
-            self.cityNameLabel.text = weatherVM.name.value
-            self.currnetTemperatureLabel.text =  "\(weatherVM.currentTemperature.temperature.value.formatAsDegree)"
-            self.maxTempLabel.text = "\(weatherVM.currentTemperature.temperatureMax.value.formatAsDegree)"
-            self.minTempLabel.text = "\(weatherVM.currentTemperature.temperatureMin.value.formatAsDegree)"
+            weatherVM.name.bind{ self.cityNameLabel.text = $0}
+            weatherVM.currentTemperature.temperature.bind{ self.currnetTemperatureLabel.text = $0.formatAsDegree}
+            weatherVM.currentTemperature.temperatureMax.bind{ self.maxTempLabel.text = $0.formatAsDegree}
+            weatherVM.currentTemperature.temperatureMin.bind{ self.minTempLabel.text = $0.formatAsDegree}
         }
-        
+
+        // change value
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+            self.weatherViewModel?.name.value = "Chanage Value after 2 sec"
+        }
     }
     
 }
