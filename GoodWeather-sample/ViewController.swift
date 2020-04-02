c35e3533e8922a0a44697f7d1b4392f0a6053073
a1824jj <a1824jj@aiit.ac.jp>
2020-04-02 10:14:09 +0900

    setup project

---
+++ GoodWeather-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeather-sample
+//
+//  Created by JONGWOO JIN on 2020/04/02.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
