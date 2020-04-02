10a317efef8c09f6118e7da0fad1b80d06d9ef32
a1824jj <a1824jj@aiit.ac.jp>
2020-04-02 10:42:29 +0900

    add weatherCell & hardcoding

---
+++ GoodWeather-sample/Custom Cells/WeatherCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherCell.swift
+//  GoodWeather-sample
+//
+//  Created by JONGWOO JIN on 2020/04/02.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    
+}
