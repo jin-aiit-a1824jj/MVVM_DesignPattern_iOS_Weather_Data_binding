dac1fd9c9e58d4c62192e39a3490e181035221a7
a1824jj <a1824jj@aiit.ac.jp>
2020-04-02 13:05:29 +0900

    setting settingsTableViewController

--- GoodWeather-sample/Controllers/SettingsTableViewController.swift
+++ GoodWeather-sample/Controllers/SettingsTableViewController.swift
@@ -16,4 +16,20 @@ class SettingsTableViewController: UITableViewController{
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+    private var settingsViewModel = SettingsViewModel()
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.settingsViewModel.units.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let settingsItem = self.settingsViewModel.units[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
+        cell.textLabel?.text = settingsItem.displayName
+        return cell
+    }
 }
