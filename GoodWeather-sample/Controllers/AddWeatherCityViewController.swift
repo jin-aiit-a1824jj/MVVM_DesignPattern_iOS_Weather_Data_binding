fecab121a79570d5fa1d4fa285767cd2f467bdd1
a1824jj <a1824jj@aiit.ac.jp>
2020-04-02 10:27:05 +0900

    set uinavigationbar theme

---
+++ GoodWeather-sample/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather-sample
+//
+//  Created by JONGWOO JIN on 2020/04/02.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather-sample/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather-sample
+//
+//  Created by JONGWOO JIN on 2020/04/02.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+}
